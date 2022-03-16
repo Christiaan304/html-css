8aacededf646877104d58084490e02522f6a7b49
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-15 22:42:53 -0300

    .

--- projeto-site-calculadora/script/script.js
+++ projeto-site-calculadora/script/script.js
@@ -21,25 +21,30 @@ function calcular()
     }
     else
     {
+        let massa_bh = Number(massa.value) * constantes.massa_solar
+
         let res_r = document.querySelector(`td#res_rs`)
-        let raio = Number(massa.value) * constantes.massa_solar * ( (2 * constantes.G) / constantes.c ** 2 )
+        let raio = massa_bh * ( (2 * constantes.G) / constantes.c ** 2 )
         res_rs.innerHTML += `${raio}`
 
         let res_as = document.querySelector(`td#res_as`)
-        let area_superficie = Number(massa.value) * constantes.massa_solar ** 2 * ( (16 * constantes.pi * constantes.G ** 2) / constantes.c ** 4 )
+        let area_superficie = massa_bh ** 2 * ( (16 * constantes.pi * constantes.G ** 2) / constantes.c ** 4 )
         res_as.innerHTML += `${area_superficie}`
 
         let res_densidade = document.querySelector(`td#res_densidade`)
-        let densidade =  (3 * constantes.c ** 6) / (32 * constantes.pi * constantes.G ** 3 * Number(massa.value) * constantes.massa_solar ** 2)
+        let densidade =  (3 * constantes.c ** 6) / (32 * constantes.pi * constantes.G ** 3 * massa_bh ** 2)
         res_densidade.innerHTML += `${densidade}`
 
         let res_gs = document.querySelector(`td#res_gs`)
-        let gravidade_superficie = (constantes.c ** 4) / (4 * constantes.G * Number(massa.value) * constantes.massa_solar)
+        let gravidade_superficie = (constantes.c ** 4) / (4 * constantes.G * massa_bh)
         res_gs.innerHTML += `${gravidade_superficie}`
 
         let res_st = document.querySelector(`td#res_st`)
-        let mare = (constantes.c ** 6) / (4 * constantes.G ** 2 * (Number(massa.value) * constantes.massa_solar) ** 2)
+        let mare = (constantes.c ** 6) / (4 * constantes.G ** 2 * (massa_bh) ** 2)
         res_st.innerHTML += `${mare}`
-        
+
+        let res_ts = document.querySelector(`td#res_ts`)
+        let tempo_singularidade = massa_bh * ( constantes.pi * constantes.G / constantes.c ** 3 )
+        res_ts.innerHTML += `${tempo_singularidade}`
     }
 }
