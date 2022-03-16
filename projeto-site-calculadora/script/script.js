1fdda0a24aa6fb31ec43052e9a73c8f086f6b523
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-15 22:50:52 -0300

    .

--- projeto-site-calculadora/script/script.js
+++ projeto-site-calculadora/script/script.js
@@ -5,6 +5,7 @@ let constantes =
     c : 299792458,
     G : 6.67408e-11,
     pi : 3.14159,
+    hbar : 1.05457e-34,
     massa_solar : 1.98847e30
 }
 
@@ -31,20 +32,26 @@ function calcular()
         let area_superficie = massa_bh ** 2 * ( (16 * constantes.pi * constantes.G ** 2) / constantes.c ** 4 )
         res_as.innerHTML += `${area_superficie}`
 
-        let res_densidade = document.querySelector(`td#res_densidade`)
+        let res_density = document.querySelector(`td#res_density`)
         let densidade =  (3 * constantes.c ** 6) / (32 * constantes.pi * constantes.G ** 3 * massa_bh ** 2)
-        res_densidade.innerHTML += `${densidade}`
+        res_density.innerHTML += `${densidade}`
 
         let res_gs = document.querySelector(`td#res_gs`)
         let gravidade_superficie = (constantes.c ** 4) / (4 * constantes.G * massa_bh)
         res_gs.innerHTML += `${gravidade_superficie}`
 
         let res_st = document.querySelector(`td#res_st`)
-        let mare = (constantes.c ** 6) / (4 * constantes.G ** 2 * (massa_bh) ** 2)
+        let mare = (constantes.c ** 6) / (4 * constantes.G ** 2 * massa_bh ** 2)
         res_st.innerHTML += `${mare}`
 
         let res_ts = document.querySelector(`td#res_ts`)
         let tempo_singularidade = massa_bh * ( constantes.pi * constantes.G / constantes.c ** 3 )
         res_ts.innerHTML += `${tempo_singularidade}`
+
+        let res_entropy = document.querySelector(`td#res_entropy`)
+        let entropy = massa_bh ** 2 * ( 4 * constantes.pi * constantes.G / constantes.c *  constantes.hbar)
+        res_entropy.innerHTML += `${entropy}`
+
+        
     }
 }
