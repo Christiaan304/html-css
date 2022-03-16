dd4764cf75eb46dbbb2b791b44f387e9af0e1334
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-15 21:40:49 -0300

    .

--- projeto-site-calculadora/script/script.js
+++ projeto-site-calculadora/script/script.js
@@ -1,7 +1,10 @@
 let massa = document.querySelector(`input#massa_txt`)
+let res_r = document.querySelector(`div#res_r`)
+
 let constantes = 
 {
     c : 299792458,
+    massa_solar : 1.98847e30,
     G : 6.67408e-11
 }
 
@@ -13,7 +16,8 @@ function calcular()
     }
     else
     {
-        let raio = massa * ( (2 * constantes.G) / constantes.c ** 2 )
-        
+        let raio = Number(massa.value) * constantes.massa_solar * ( (2 * constantes.G) / constantes.c ** 2 )
+        res_r.innerHTML += `${raio}`
+
     }
 }
