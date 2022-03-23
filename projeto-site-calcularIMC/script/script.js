76018128b9bbb207177de88930e58717484996bd
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-03-23 05:24:04 -0300

    ;

--- projeto-site-calcularIMC/script/script.js
+++ projeto-site-calcularIMC/script/script.js
@@ -33,34 +33,49 @@ function verificarIMC(imc)
 }
 */
 
-$(`#calcular`).click(function(){
-    let peso = $(`#pesotxt`).val() /*document.querySelector(`input#pesotxt`)*/
-    let altura = $(`#alturatxt`).val() /*document.querySelector(`input#alturatxt`)*/
-    let imc = ( Number(peso) / ( Math.pow(altura / 100, 2) ) ).toFixed(2)
+$(document).ready(function(){
 
-    verificarIMC(imc)
-});
+    $(`input#calcular`).click(function calcularIMC(){
+        let peso = $(`#pesotxt`).val() /*document.querySelector(`input#pesotxt`)*/
+        let altura = $(`#alturatxt`).val() /*document.querySelector(`input#alturatxt`)*/
+        let imc = ( Number(peso) / ( Math.pow(altura / 100, 2) ) ).toFixed(2)
 
-function verificarIMC(imc)
-{
-    if ( imc < 18.4 )
+        verificarDados(peso, altura, imc)
+    })
+
+    function verificarDados(peso, altura, imc)
     {
-        res.innerHTML = ``
-        res.innerHTML += `<p>Seu imc é ${imc}, você está abaixo do peso</p>`
+        if ( peso.length == 0 || altura.length == 0 )
+        {
+            alert(`[ERRO] Altura ou Peso não podem ficar vazios.`)
+        }
+        else
+        {
+            verificarIMC(imc)
+        }
     }
-    else if ( imc < 24.9 )
+
+    function verificarIMC(imc)
     {
-        res.innerHTML = ``
-        res.innerHTML += `<p>Seu imc é ${imc}, você está com o peso normal</p>`
+        if ( imc < 18.4 )
+        {
+            res.innerHTML = ``
+            res.innerHTML = `<p>Seu IMC é <strong>${imc}</strong>, você está abaixo do peso</p>`
+        }
+        else if ( imc < 24.9 )
+        {
+            res.innerHTML = ``
+            res.innerHTML = `<p>Seu IMC é <strong>${imc}</strong>, você está com o peso normal</p>`
+        }
+        else if ( imc < 29.9 )
+        {
+            res.innerHTML = ``
+            res.innerHTML = `<p>Seu IMC é <strong>${imc}</strong>, você está acima do peso</p>`
+        }
+        else
+        {
+            res.innerHTML = ``
+            res.innerHTML = `<p>Seu IMC é <strong>${imc}</strong>, você está obeso</p>`
+        }
     }
-    else if ( imc < 29.9 )
-    {
-        res.innerHTML = ``
-        res.innerHTML += `<p>Seu imc é ${imc}, você está acima do peso</p>`
-    }
-    else
-    {
-        res.innerHTML = ``
-        res.innerHTML += `<p>Seu imc é ${imc}, você está obeso</p>`
-    }
-}
+})
