cf531e216bb4a834ee1630d9073a28e261e28bb6
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-05-16 20:47:20 -0300

    Update script.js

--- projetos-sites/projeto-site-planetas/js/script.js
+++ projetos-sites/projeto-site-planetas/js/script.js
@@ -9,7 +9,7 @@ function update()
     switch(value)
     { 
         case 1:
-            img.src = `../images/sun.jpg`
+            img.src = `../../images/sun.jpg`
             img.alt = `imgaem do sol`
 
             information.innerHTML = `<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ipsam officia, nam deleniti earum
@@ -18,70 +18,70 @@ function update()
         break
 
         case 2:
-            img.src = `../images/mercury.jpg`
+            img.src = `../../images/mercury.jpg`
             img.alt = `imgaem do planeta mercúrio`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 3:
-            img.src = `../images/venus.jpg`
+            img.src = `../../images/venus.jpg`
             img.alt = `imgaem do planeta venus`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 4:
-            img.src = `../images/earth.jpg`
+            img.src = `../../images/earth.jpg`
             img.alt = `imgaem do planeta terra`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 5:
-            img.src = `../images/moon.jpg`
+            img.src = `../../images/moon.jpg`
             img.alt = `imgaem da lua`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 6:
-            img.src = `../images/mars.jpg`
+            img.src = `../../images/mars.jpg`
             img.alt = `imgaem do planeta marte`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 7:
-            img.src = `../images/asteroidBelt.png`
+            img.src = `../../images/asteroidBelt.png`
             img.alt = `imgaem do cinturão de asteróides`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 8:
-            img.src = `../images/vesta.jpg`
+            img.src = `../../images/vesta.jpg`
             img.alt = `imgaem do planeta anão vesta`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 9:
-            img.src = `../images/ceres.png`
+            img.src = `../../images/ceres.png`
             img.alt = `imgaem do planeta anão ceres`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores? acer</p>`
         break
 
         case 10:
-            img.src = `../images/jupiter.jpg`
+            img.src = `../../images/jupiter.jpg`
             img.alt = `imgaem do planeta júpiter`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 11:
-            img.src = `../images/saturn.jpg`
+            img.src = `../../images/saturn.jpg`
             img.alt = `imgaem do planeta saturno`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
@@ -89,7 +89,7 @@ function update()
         break
 
         case 12:
-            img.src = `../images/uranus.jpg`
+            img.src = `../../images/uranus.jpg`
             img.alt = `imgaem do planeta urano`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
@@ -97,7 +97,7 @@ function update()
         break
 
         case 13:
-            img.src = `../images/neptune.jpg`
+            img.src = `../../images/neptune.jpg`
             img.alt = `imgaem do planeta netuno`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
@@ -105,7 +105,7 @@ function update()
         break
 
         case 14:
-            img.src = `../images/pluto.jpg`
+            img.src = `../../images/pluto.jpg`
             img.alt = `imgaem do planeta anão plutão`
 
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
