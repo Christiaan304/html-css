f34d1ed9527446f5031945ea3435882c3c03d5b2
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-05-15 22:28:52 -0300

    .

--- projetos-sites/projeto-site-planetas/js/script.js
+++ projetos-sites/projeto-site-planetas/js/script.js
@@ -25,51 +25,91 @@ function update()
         break
 
         case 3:
-            
+            img.src = `../images/venus.jpg`
+            img.alt = `imgaem do planeta venus`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 4:
-            
+            img.src = `../images/earth.jpg`
+            img.alt = `imgaem do planeta terra`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 5:
-            
+            img.src = `../images/moon.jpg`
+            img.alt = `imgaem da lua`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 6:
-            
+            img.src = `../images/mars.jpg`
+            img.alt = `imgaem do planeta marte`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 7:
-            
+            img.src = `../images/asteroidBelt.png`
+            img.alt = `imgaem do cinturão de asteróides`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 8:
-            
+            img.src = `../images/vesta.jpg`
+            img.alt = `imgaem do planeta anão vesta`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 9:
-            
+            img.src = `../images/ceres.png`
+            img.alt = `imgaem do planeta anão ceres`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores? acer</p>`
         break
 
         case 10:
-            
+            img.src = `../images/jupiter.jpg`
+            img.alt = `imgaem do planeta júpiter`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
         case 11:
-            
+            img.src = `../images/saturn.jpg`
+            img.alt = `imgaem do planeta saturno`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
+            Odit quas laboriosam repellat labore? Nisi, repudiandae. Quasi odio quod rerum sunt laborum repudiandae eligendi sed sapiente illo facilis blanditiis iusto, iste reiciendis ratione magnam officiis aliquid incidunt nulla harum.</p>`
         break
 
         case 12:
-            
+            img.src = `../images/uranus.jpg`
+            img.alt = `imgaem do planeta urano`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
+            Odit quas laboriosam repellat labore? Nisi, repudiandae. Quasi odio quod rerum sunt laborum repudiandae eligendi sed sapiente illo facilis blanditiis iusto, iste reiciendis ratione magnam officiis aliquid incidunt nulla harum.</p>`
         break
 
         case 13:
-            
+            img.src = `../images/neptune.jpg`
+            img.alt = `imgaem do planeta netuno`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
+            Odit quas laboriosam repellat labore? Nisi, repudiandae. Quasi odio quod rerum sunt laborum repudiandae eligendi sed sapiente illo facilis blanditiis iusto, iste reiciendis ratione magnam officiis aliquid incidunt nulla harum.</p>`
         break
 
         case 14:
-            
+            img.src = `../images/pluto.jpg`
+            img.alt = `imgaem do planeta anão plutão`
+
+            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita unde natus explicabo tenetur rem dolorem, inventore quo saepe ipsa nobis laudantium labore possimus, quod blanditiis. Fuga tempora distinctio saepe voluptatibus.
+            Odit quas laboriosam repellat labore? Nisi, repudiandae. Quasi odio quod rerum sunt laborum repudiandae eligendi sed sapiente illo facilis blanditiis iusto, iste reiciendis ratione magnam officiis aliquid incidunt nulla harum.</p>`
         break
 
         case 15:
