5a1bddfb3745a22177914e6c9931ccb467d92a60
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-05-16 23:20:48 -0300

    .

--- projetos-sites/projeto-site-planetas/script.js
+++ projetos-sites/projeto-site-planetas/script.js
@@ -1,3 +1,9 @@
+resumo = {
+    sun:`<p>O <span class="fw-bold">Sol</span> é a estrela central do sistema solar. Todos os outros corpos do Sistema Solar, como planetas, planetas anões, asteroides, cometas e poeira, giram ao seu redor. Ele é responsável por 99,86% da massa do Sistema Solar, a distância da Terra ao Sol é de cerca de 150 milhões de quilômetros ou 1 unidade astronômica e a luz solar demora aproximadamente 8 minutos e 18 segundos para chegar à Terra. É composto primariamente de hidrogênio (74% de sua massa) e hélio (24% da sua massa), com traços de outros elementos, incluindo ferro, níquel, oxigênio, silício, enxofre, magnésio, néon, cálcio e crômio. O Sol orbita em torno do centro da Via Láctea a uma velocidade de 250 km/s, atravessando no momento a Nuvem Interestelar Local de gás de alta temperatura, no interior do Braço de Órion da Via Láctea, entre os braços maiores Perseus e Sagitário.</p>`,
+
+    mercury:`<p><span class="fw-bold">Mercúrio</span> é o menor e o primeiro planeta do Sistema Solar, orbitando o Sol a cada 87,97 dias terrestres e demora 56,64 dias para completar uma volta no seu eixo. Tem uma aparência similar à da Lua com crateras de impacto e planícies lisas, não possui satélites naturais e atmosfera, a temperatura em sua superfície varia de −183 °C a 427 °C.</p>`
+}
+
 function update() 
 {
     let select = document.querySelector(`#solarSystemObjects`)
@@ -11,23 +17,18 @@ function update()
         case 1:
             img.src = `./images/sun.jpg`
             img.alt = `imgaem do sol`
-
-            information.innerHTML = `<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ipsam officia, nam deleniti earum
-            dignissimos atque, corporis dolores voluptas excepturi similique ratione repudiandae laboriosam repellat
-            soluta facilis quaerat sint exercitationem!</p>`
+            information.innerHTML = resumo.sun
         break
 
         case 2:
             img.src = `./images/mercury.jpg`
             img.alt = `imgaem do planeta mercúrio`
-
-            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
+            information.innerHTML = resumo.mercury
         break
 
         case 3:
             img.src = `./images/venus.jpg`
             img.alt = `imgaem do planeta venus`
-
             information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
         break
 
