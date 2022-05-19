df3f4d65d5827a25c31346bbd343d687e2eb2067
christiaan <36467351+Christiaan304@users.noreply.github.com>
2022-05-18 21:09:54 -0300

    Update script.js

--- projetos-sites/projeto-site-planetas/script.js
+++ projetos-sites/projeto-site-planetas/script.js
@@ -5,7 +5,9 @@ resumo = {
 
     venus:`<p><span class="fw-bold">Vênus</span> é o segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando a cada 224,7 dias. Recebeu seu nome em homenagem à deusa romana do amor e da beleza Vénus, equivalente a Afrodite. Depois da Lua, é o objeto mais brilhante do céu noturno, é considerado um planeta do tipo terrestre ou telúrico, chamado com frequência de planeta irmão da Terra, já que são similares quanto ao tamanho, massa e composição. Vénus é coberto por uma camada opaca de nuvens de ácido sulfúrico altamente reflexivas, impedindo que a sua superfície seja vista do espaço na luz visível. Ele possui a mais densa atmosfera entre todos os planetas terrestres do Sistema Solar, constituída principalmente de dióxido de carbono e a pressão atmosférica na superfície do planeta é 92 vezes a da Terra.</p>`,
 
-    earth:`<p>A <span class="fw-bold">Terra</span> é o terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar,  orbita o Sol a cada 365,2 dias, e é o único corpo celeste onde é conhecida a existência de vida. A sua superfície exterior está dividida em vários segmentos rígidos, chamados placas tectônicas, que migram sobre a superfície terrestre ao longo de milhões de anos. Cerca de 71% da superfície da Terra está coberta por oceanos de água salgada, com o restante consistindo de continentes e ilhas. O eixo de rotação da Terra possui uma inclinação de 23,4°, produzindo as estações do ano. A Lua é o único satélite natural conhecido da Terra, é responsável pelas marés, estabiliza a inclinação axial da Terra e desacelera gradualmente a rotação do planeta.</p>`
+    earth:`<p>A <span class="fw-bold">Terra</span> é o terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar,  orbita o Sol a cada 365,2 dias, e é o único corpo celeste onde é conhecida a existência de vida. A sua superfície exterior está dividida em vários segmentos rígidos, chamados placas tectônicas, que migram sobre a superfície terrestre ao longo de milhões de anos. Cerca de 71% da superfície da Terra está coberta por oceanos de água salgada, com o restante consistindo de continentes e ilhas. O eixo de rotação da Terra possui uma inclinação de 23,4°, produzindo as estações do ano. A Lua é o único satélite natural conhecido da Terra, é responsável pelas marés, estabiliza a inclinação axial da Terra e desacelera gradualmente a rotação do planeta.</p>`,
+
+    moon:`<p>A <span class="fw-bold">Lua</span> é o único satélite natural da Terra e o quinto maior do Sistema Solar, demora 29,5 dias para completar uma orbita ao redor da Terra. É o maior satélite natural de um planeta no sistema solar em relação ao tamanho do seu corpo primário, tendo 27% do diâmetro e 60% da densidade da Terra. A explicação mais atual da sua formação é que a Lua tenha sido formada a partir dos detritos de um impacto de proporções gigantescas entre a Terra e um outro corpo do tamanho de Marte. A Lua encontra-se em rotação sincronizada com a Terra, mostrando sempre a mesma face visível, marcada por mares vulcânicos escuros entre montanhas e crateras de impacto.</p>`
 }
 
 function update() 
@@ -47,7 +49,7 @@ function update()
             img.src = `./images/moon.jpg`
             img.alt = `imgaem da lua`
 
-            information.innerHTML = `<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ex, inventore nesciunt aut modi explicabo repudiandae, vel beatae quo cumque ducimus adipisci. Officiis velit exercitationem hic, aliquam numquam nemo asperiores?</p>`
+            information.innerHTML = resumo.moon
         break
 
         case 6:
